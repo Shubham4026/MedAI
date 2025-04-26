e7686c136f5201cfc4402b04558e2dddcac48074
Shubham <shubham.jha@tekditechnologies.com>
2025-04-26 14:52:09 +0530

    Added Setup Auth

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
+import { setupAuth } from "./auth";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  setupAuth(app);
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
